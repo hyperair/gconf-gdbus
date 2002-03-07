2666ec68823d694025821f9af4834127ef80bd49
Christian Rose <menthos@menthos.com>
2002-03-07 01:31:46 +0000

    Removed these scripts. They are obsoleted by intltool.
    2002-03-07  Christian Rose  <menthos@menthos.com>
    
    	* update.pl, update.sh: Removed these scripts. They are obsoleted
    	by intltool.

--- po/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-xgettext --default-domain=GConf --directory=.. \
-  --add-comments --keyword=_ --keyword=N_ \
-  --files-from=./POTFILES.in \
-&& test ! -f GConf.po \
-   || ( rm -f ./GConf.pot \
-    && mv GConf.po ./GConf.pot )
