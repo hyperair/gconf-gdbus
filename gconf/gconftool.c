7d69f63db13a27a98db621205e191ddb4ad39978
Mark McLoughlin <mark@skynet.ie>
2004-04-01 14:44:22 +0000

    don't dump the value if its the schema default.
    2004-04-01  Mark McLoughlin  <mark@skynet.ie>
    
            * gconf/gconftool.c: (dump_entries_in_dir): don't dump
            the value if its the schema default.

--- gconf/gconftool.c
+++ gconf/gconftool.c
@@ -1497,7 +1497,8 @@ dump_entries_in_dir(GConfEngine* conf, const gchar* dir, const gchar* base_dir)
         g_print ("      <schema_key>%s</schema_key>\n",
 		 get_key_relative(gconf_entry_get_schema_name(entry), base_dir));
 
-      if (entry->value)
+      /* don't print the value if its just the schema default */
+      if (entry->value && !gconf_entry_get_is_default(entry))
         print_value_in_xml(entry->value, 6);
 
       g_print ("    </entry>\n");
