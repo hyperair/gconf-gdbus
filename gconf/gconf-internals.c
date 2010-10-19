d6e7030cffd5d868c11b03858bebdac34e652c5e
Josselin Mouette <joss@debian.org>
2010-10-19 07:57:25 +0200

    Bug #631250 - obsolete error message

--- gconf/gconf-internals.c
+++ gconf/gconf-internals.c
@@ -2874,7 +2874,7 @@ gconf_activate_server (gboolean  start_if_not_found,
     g_set_error (error,
                  GCONF_ERROR,
                  GCONF_ERROR_NO_SERVER,
-                 _("Failed to contact configuration server; some possible causes are that you need to enable TCP/IP networking for ORBit, or you have stale NFS locks due to a system crash. See http://projects.gnome.org/gconf/ for information. (Details - %s)"),
+                 _("Failed to contact configuration server; the most common cause is a missing or misconfigured D-Bus session bus daemon. See http://projects.gnome.org/gconf/ for information. (Details - %s)"),
                  failure_log->len > 0 ? failure_log->str : _("none"));
 
   g_string_free (failure_log, TRUE);
